1bdc30de251d3295aaa071790d0ec4f7512dd4af
EdenCoder <alex@adlist.co.nz>
2019-04-01 14:07:40 +1000

    amend admin grid

--- bundles/affiliate/controllers/affiliate.js
+++ bundles/affiliate/controllers/affiliate.js
@@ -528,7 +528,7 @@ class AffiliateController extends Controller {
 
     // add refund grid
     creditGrid.where({
-      'affiliate.id' : affiliate.get('_id').toString(),
+      'affiliate.id' : affiliate ? affiliate.get('_id').toString() : 'null',
     });
 
     // return grid
